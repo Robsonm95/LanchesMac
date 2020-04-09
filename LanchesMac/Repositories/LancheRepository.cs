d809fa94f6a91392477e72bd60cb60e817e7632d
robson-messias <63297156+robson-messias@users.noreply.github.com>
2020-04-09 12:01:27 -0300

    Criado o repositorio

---
+++ LanchesMac/Repositories/LancheRepository.cs
@@ -0,0 +1,31 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMac.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches
+            .Include(c => c.Categoria)
+            .Where(p => p.IsLanchePreferido);
+
+        public Lanche GetLancheById(int LancheId)
+        {
+            return _context.Lanches.FirstOrDefault(l => l.LancheId == LancheId);
+        }
+    }
+}
--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using LanchesMac.Context;
+using LanchesMac.Repositories;
 
 namespace LanchesMac
 {
@@ -25,10 +26,13 @@ namespace LanchesMac
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-
             services.AddDbContext<AppDbContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("AppDbContext")));
+
+            services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+            services.AddTransient<ILancheRepository, LancheRepository>();
+            
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
